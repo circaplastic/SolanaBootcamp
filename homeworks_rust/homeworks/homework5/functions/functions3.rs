6b69aee44aaa502c0fc24a65f3ca875b701eba92
circaplastic <circaplastic@gmail.com>
2025-06-09 19:00:14 -0600

    added solutions to functions2-5

--- homeworks_rust/homeworks/homework5/functions/functions3.rs
+++ homeworks_rust/homeworks/homework5/functions/functions3.rs
@@ -1,10 +1,10 @@
 // functions3.rs
 // Make me compile! Execute `rustlings hint functions3` for hints :)
 
-// I AM NOT DONE
+
 
 fn main() {
-    call_this();
+    call_this(7);
 }
 
 fn call_this(num: u32) {
@@ -12,3 +12,5 @@ fn call_this(num: u32) {
         println!("Loop now {}", i + 1);
     }
 }
+
+// SOLution
